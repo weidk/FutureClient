a734cb6d3fd5d181194bcc8be57a4ca85815d79d
wdk01 <599061036@qq.com>
2021-01-08 09:58:35 +0800

    期货交易客户端模块WPF

---
+++ Models/FutureCode.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureClient.Models
+{
+    public class FutureCode
+    {
+        public string Code { get; set; }
+    }
+}
